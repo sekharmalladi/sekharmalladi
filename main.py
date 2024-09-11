eda856734748406eaa4d6e53f78bdc229493807c
sekharmalladi <somasekharmalladi5@gmail.com>
2024-09-11 17:37:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import qrcode
+url = "https://www.youtube.com"
+
+qr = qrcode.QRCode(
+    version=1, 
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,  
+)
+qr.add_data(url)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("website_Qr_code.png")
+
